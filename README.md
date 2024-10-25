f21e01e587878f4a809fba8c2f5b85ad3ffb5836
klose <mingrenjbdt@163.com>
2024-10-25 18:38:15 +0800

    feat: update files

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # open-code-with-content
 
 使用URL scheme打开vscode并打开带有内容的新文件
+
+```html
+<script>
+  function encodeBase64(content) {
+    return btoa(encodeURIComponent(content))
+  }
+
+  const vscodeUrl = `vscode://mingneo.vscode-open-code-with-content?content=${encodeBase64('Content Text')}&language=plaintext`
+</script>
+
+<a href="vscodeUrl">open vscode with content</a>
+```
--- src/extension.ts
+++ src/extension.ts
@@ -3,25 +3,26 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
   const uriHandler = vscode.window.registerUriHandler({
     handleUri(uri) {
-      return handleCreateFiles(uri);
+      return handleOpenFiles(uri);
     },
   });
   context.subscriptions.push(uriHandler);
 
   return {
-    handleUri: handleCreateFiles,
+    handleUri: handleOpenFiles,
   };
 }
 
 const decodeBase64 = (data: string): string => {
-  // return Buffer.from(data, 'base64').toString('ascii');
-  return Buffer.from(data, 'base64').toString('utf8');
+  return Buffer.from(data, 'base64').toString('ascii');
+  // return Buffer.from(data, 'base64').toString('utf8');
 };
 
-async function handleCreateFiles(uri: vscode.Uri) {
+async function handleOpenFiles(uri: vscode.Uri) {
   try {
     const params = new URLSearchParams(uri.query);
     const contents = params.getAll('content');
+    const languages = params.getAll('language') || 'plaintext';
     if (contents.length === 0) {
       vscode.window.showErrorMessage('URL中没有content参数');
       return;
@@ -32,11 +33,11 @@ async function handleCreateFiles(uri: vscode.Uri) {
       return;
     }
     for (let i = 0; i < contents.length; i++) {
-      const decodedContent = sanitizeContent(decodeURIComponent(decodeBase64(contents[i])));
+      const decodedContent = formatContent(decodeURIComponent(decodeBase64(contents[i])));
       if (!decodedContent.trim()) {
         continue;
       }
-      const document = await vscode.workspace.openTextDocument({ content: decodedContent });
+      const document = await vscode.workspace.openTextDocument({ content: decodedContent, language: languages[i] });
       await vscode.window.showTextDocument(document, { preview: false });
     }
   } catch (error) {
@@ -44,7 +45,7 @@ async function handleCreateFiles(uri: vscode.Uri) {
   }
 }
 
-function sanitizeContent(content: string): string {
+function formatContent(content: string): string {
   return content.replace(/[\x00-\x09\x0B\x0C\x0E-\x1F\x7F]/g, '');
 }
 
